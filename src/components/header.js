341b8b1bd073cfe4ecef4bedde9a60d7f6b2a749
henrique.dev <pedro.dev.public2022@gmail.com>
2023-02-18 10:09:12 -0300

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const header = () => {
+    return (
+        <header className="header">
+            <p>StarCode</p>
+            <div className="links">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+            </div>
+        </header>
+    );
+}
+
+export default header;
---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Main () {
+    return (
+        <main className="mainContent">
+            <section className="animeContent">
+                Contéudo
+            </section>
+            <section className="animesPopulares">
+                Populares
+            </section>
+        </main>
+    );
+}
+
+export default Main;
